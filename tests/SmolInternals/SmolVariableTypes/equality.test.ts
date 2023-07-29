3f04523b556ecc63203c4b89d5a0f1ddad9896d4
Adrian O'Connor <adrian.oconnor@gmail.com>
2023-07-29 16:31:20 +0100

    Expanding equality tests

--- tests/SmolInternals/SmolVariableTypes/equality.test.ts
+++ tests/SmolInternals/SmolVariableTypes/equality.test.ts
@@ -1,5 +1,13 @@
 import { describe, expect, test } from '@jest/globals';
 import { SmolNumber } from '../../../src/Internals/SmolVariableTypes/SmolNumber'
+import { SmolBool } from '../../../src/Internals/SmolVariableTypes/SmolBool';
+
+/*
+These tests check how the Smol 'wrapped' variable types deal with equality checks in
+the host runtime. This is less of an issue for the JS version, because each types'
+stored 'value' can be compared directly and we'll just magically inherit the JS
+behaviour from the host -- it's more of an issue for .net
+*/
 
 describe('SmolInteral VariableTypes', () => {
   test('SmolNumber Equality', () => {
@@ -11,5 +19,15 @@ describe('SmolInteral VariableTypes', () => {
     expect(n1.equals(n2)).toBeTruthy(); // This is using our own equality method on the base type
     expect(n1.equals(n3)).toBeFalsy();
     expect(n1 == n2).toBeFalsy(); // This is comparing object instances, which are not the same
+
+    const b1 = new SmolBool(true);
+    const b2 = new SmolBool(true);
+    const b3 = new SmolBool(false);
+    
+    expect(b1.equals(b2)).toBeTruthy();
+    expect(b1.equals(b3)).toBeFalsy();
+    expect(b1 == b2).toBeFalsy();
+    expect(b1.getValue()).toBeTruthy();
+    expect(b3.getValue()).toBeFalsy();
   });
 });
